bbfa56fb184d389bc5fe01638192ae71f019a21c
rcs <rolf.c.stadheim@gmail.com>
2015-01-22 16:47:18 +0100

    Corrected shear test for vcd

--- t/TestBeams.hs
+++ t/TestBeams.hs
@@ -21,8 +21,8 @@ testBeams = test [
                     assertEqual "[B1] mcd" 193.6 (ro2dec (B.mcd b) 1)
                     assertEqual "[B1] as links" 100.5 (ro2dec (B.asLink (B.links b)) 1)
                     let Just ccLinks = B.ccLinks b 100 90.4
-                    assertEqual "[B1] cc links" 190.8 (ro2dec ccLinks 1)
+                    -- assertEqual "[B1] cc links" 190.8 (ro2dec ccLinks 1)
+                    assertEqual "[B1] cc links" 422.9 (ro2dec ccLinks 1)
                     assertEqual "[B1] min as links" 204.0 (ro2dec (B.minAsLinks b) 1)
-                    -- assertEqual "[B1] min cc links" 492.8 (ro2dec (B.minCcLinks b) 1)
                     assertEqual "[B1] min cc links" 262.8 (ro2dec (B.minCcLinks b) 1)
                 ]
